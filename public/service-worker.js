7e577397d36a8ee3f2e13b26b8d96c4eb88f065e
Dimas Pamungkas Mursidi <dmspmksmrsdi@gmail.com>
2024-08-24 10:33:58 +0700

    Fixing BUG

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,20 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('v1').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/favicon.png',
-                '/src/main.jsx'
-            ]);
-        })
-    );
-});
+// self.addEventListener('install', event => {
+//     event.waitUntil(
+//         caches.open('v1').then(cache => {
+//             return cache.addAll([
+//                 '/',
+//                 '/index.html',
+//                 '/favicon.png',
+//                 '/src/main.jsx'
+//             ]);
+//         })
+//     );
+// });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
+// self.addEventListener('fetch', event => {
+//     event.respondWith(
+//         caches.match(event.request).then(response => {
+//             return response || fetch(event.request);
+//         })
+//     );
+// });
